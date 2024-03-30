725ca66f3c706adea96d66e4d70a6022aea39f8a
Sharma <Prabha_Sharma@DELL.com>
2024-03-30 20:47:57 +0530

    added code for missing value

--- data_analyis.py
+++ data_analyis.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+output_folder = 'plots'
+
 def load_data(file_path):
     """Load data from a CSV file."""
     try:
@@ -21,26 +23,43 @@ def analyze_data(data):
         print(data.describe())
 
         # Plot histograms for numeric columns
-        print("Histograms:")
+        print("Plotting Histograms:")
         for col in data.select_dtypes(include=['int', 'float']):
             data[col].plot(kind='hist', bins=10)
             plt.title(col)
             plt.xlabel(col)
             plt.ylabel('Frequency')
-            plt.show()
-        
+            plt.savefig(f'{output_folder}/histogram_{col}.png', )
+            #plt.show()
+            plt.close()
+
         # Plot bar plot for the class label (string type)
         class_label_counts = data['Class'].value_counts()
         class_label_counts.plot(kind='bar')
         plt.title('Class Label Distribution')
         plt.xlabel('Class Label')
         plt.ylabel('Count')
-        plt.show()
+        plt.savefig(f'{output_folder}/class_label.png', )
+        plt.close()
+        #plt.show()
+
+
+def findMissingData(data):
+    df = pd.DataFrame(data)
+
+    # Finding columns with missing values
+    columns_with_missing_values = df.columns[df.isnull().any()].tolist()
+    return columns_with_missing_values
+
+
 
 def main():
     print("Starting the Data Processing")
-    file_path = input("Enter the path to the CSV file: ")
+    file_path = "DryBeanDataset\Dry_Bean_Dataset.xlsx" #("Enter the path to the CSV file: ")
+    
     data = load_data(file_path)
+    columns_with_missing_values = findMissingData(data)
+    print("List of Column with Missing Values: - ", columns_with_missing_values)
     analyze_data(data)
 
 if __name__ == "__main__":
